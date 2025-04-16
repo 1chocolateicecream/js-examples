122cfa4c518db1c02d5129d1a980b3ac8f90173a
alisa <alisa.zavjalkina@icloud.com>
2025-04-16 13:19:32 +0300

    Create add.php

---
+++ add.php
@@ -0,0 +1,90 @@
+<?php
+// 1uzd
+// echo 'bello!';
+
+// $a = ['apple', 'banana', 'minion'];
+// print_r($a);
+// var_dump($a);
+
+// 2uzd
+// for ($x = 0; $x <= 10; $x++){
+//     echo 'number ' . $x . "\n";
+// };
+
+// for ($y = 10; $y >= 0; $y--){
+//     echo 'number' . $y . "\n";
+// };
+
+// 3uzd
+// $a = ['alise', 'bobs', 'čarlijs'];
+// for ($i = 0; $i < count($a); $i++){
+//     echo $a[$i] . "\n";
+// };
+
+// 4uzd
+// function fica(){
+//     for ($x = 0; $x <= 10; $x++){
+//             echo 'number ' . $x . "\n";
+//         };
+// };
+// fica();
+
+// 5uzd
+// function numbers($sakums,$beigums){
+//     for ($i = $sakums; $i <= $beigums; $i++){
+//             echo 'number ' . $i . "\n";
+//         };
+// }
+// numbers(3,7);
+
+// 6uzd
+
+// $ae = [
+//     'name' => 'alisa',
+//     'age' => 16
+// ];
+
+// foreach ($ae as $aboutme => $value){
+//     echo $aboutme . " = " . $value . "\n";
+// };
+
+// 7uzd
+// $cars = [
+//     [
+//         "nosaukums" => "Toyota Corolla",
+//         "tips" => "Sedans",
+//         "ipasnieki" => ["Jānis", "Anna", "Pēteris"]
+//     ],
+//     [
+//         "nosaukums" => "Volkswagen Golf",
+//         "tips" => "Hatchback",
+//         "ipasnieki" => ["Līga", "Mārtiņš"]
+//     ],
+//     [
+//         "nosaukums" => "BMW X5",
+//         "tips" => "SUV",
+//         "ipasnieki" => ["Kristīne", "Andris", "Elīna"]
+//     ]
+// ];
+
+// 8uzd
+// $html = "<ul>";
+
+// foreach ($cars as $car) {
+//     $html .= "<li>";
+//     $html .= "<strong>Nosaukums:</strong> " . $car["nosaukums"] . "<br>";
+//     $html .= "<strong>Tips:</strong> " . $car["tips"];
+
+//     $html .= "<ul>";
+//     foreach ($car["ipasnieki"] as $ipasnieks) {
+//         $html .= "<li>" . $ipasnieks . "</li>";
+//     }
+//     $html .= "</ul>";
+
+//     $html .= "</li>";
+// }
+
+// $html .= "</ul>";
+
+// echo $html;
+?>
